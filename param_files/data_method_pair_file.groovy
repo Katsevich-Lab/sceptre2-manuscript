0055822589123adc812035e95df2c92db24ae5e9
Tim <tbarry2@andrew.cmu.edu>
2022-05-24 23:16:38 -0400

    update .groovy file; examine only frangieh/control/gene and liscovitch method

--- param_files/data_method_pair_file.groovy
+++ param_files/data_method_pair_file.groovy
@@ -1,4 +1,5 @@
 // FIRST, define the dataset-method pairs to analyze in a map
+/*
 data_method_pairs = ["frangieh/co_culture/gene": ["schraivogel_method", "seurat_de"],
                      "frangieh/control/gene": ["schraivogel_method", "seurat_de"],
                      "frangieh/ifn_gamma/gene": ["schraivogel_method", "seurat_de"],
@@ -11,20 +12,25 @@ data_method_pairs = ["frangieh/co_culture/gene": ["schraivogel_method", "seurat_
                      "schraivogel/ground_truth_perturbseq/gene": ["schraivogel_method", "seurat_de"],
                      "schraivogel/ground_truth_tapseq/gene": ["schraivogel_method", "seurat_de"],
                      "simulated/experiment_1/gene": ["schraivogel_method", "seurat_de"]]
+*/
+
+data_method_pairs = ["frangieh/control/gene": ["schraivogel_method", "seurat_de"],
+                     "liscovitch/experiment_big/chromatin": ["liscovitch_method"],
+                     "liscovitch/experiment_small/chromatin": ["liscovitch_method"]]
 
 // SECOND, define a matrix indicating the amount of RAM to request for each dataset-method pair
-data_method_ram_matrix = [[34, 13],
-[23, 9],
-[36, 13],
-[2, 1],
-[2, 1],
-[18, 6],
-[2, 1],
-[6, 12],
-[8, 14],
-[44, 11],
-[2, 1],
-[28, 7]]
+data_method_ram_matrix = [[34, 13, 1],
+[23, 9, 1],
+[36, 13, 1],
+[2, 1, 1],
+[2, 1, 1],
+[18, 6, 1],
+[2, 1, 1],
+[6, 12, 1],
+[8, 14, 1],
+[44, 11, 1],
+[2, 1, 1],
+[28, 7, 1]]
 
 row_names = ["frangieh/co_culture/gene",
              "frangieh/control/gene",
@@ -40,4 +46,5 @@ row_names = ["frangieh/co_culture/gene",
              "simulated/experiment_1/gene"]
 
 col_names = ["schraivogel_method",
-             "seurat_de"]
+             "seurat_de",
+             "liscovitch_method"]
