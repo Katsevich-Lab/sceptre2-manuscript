f1e63a5dc2fdf2c9d5dd0573240e92657f0e8914
Tim <tbarry2@andrew.cmu.edu>
2022-05-28 21:52:50 -0400

    change param file: run mimosca on all methods

--- param_files/data_method_pair_file.groovy
+++ param_files/data_method_pair_file.groovy
@@ -1,4 +1,5 @@
 // FIRST, define the dataset-method pairs to analyze in a map
+
 /*
 data_method_pairs = ["frangieh/co_culture/gene": ["schraivogel_method", "seurat_de"],
                      "frangieh/control/gene": ["schraivogel_method", "seurat_de"],
@@ -13,23 +14,31 @@ data_method_pairs = ["frangieh/co_culture/gene": ["schraivogel_method", "seurat_
                      "schraivogel/ground_truth_tapseq/gene": ["schraivogel_method", "seurat_de"],
                      "simulated/experiment_1/gene": ["schraivogel_method", "seurat_de"]]
 */
+data_method_pairs = ["frangieh/co_culture/gene": ["mimosca"],
+                     "frangieh/control/gene": ["mimosca"],
+                     "frangieh/ifn_gamma/gene": ["mimosca"],
+                     "papalexi/eccite_screen/gene": ["mimosca"],
+                     "papalexi/eccite_screen/protein": ["mimosca"],
+                     "schraivogel/enhancer_screen_chr11/gene": ["mimosca"],
+                     "schraivogel/enhancer_screen_chr8/gene": ["mimosca"],
+                     "schraivogel/ground_truth_perturbseq/gene": ["mimosca"],
+                     "schraivogel/ground_truth_tapseq/gene": ["mimosca"],
+                     "simulated/experiment_1/gene": ["mimosca"]]
 
-data_method_pairs = ["liscovitch/experiment_big/chromatin": ["liscovitch_method"],
-                     "liscovitch/experiment_small/chromatin": ["liscovitch_method"]]
 
 // SECOND, define a matrix indicating the amount of RAM to request for each dataset-method pair
-data_method_ram_matrix = [[34, 13, 1],
-[23, 9, 1],
-[36, 13, 1],
-[2, 1, 1],
-[2, 1, 1],
-[18, 6, 1],
-[2, 1, 1],
-[6, 12, 1],
-[8, 14, 1],
-[44, 11, 1],
-[2, 1, 1],
-[28, 7, 1]]
+data_method_ram_matrix = [[34, 13, 1, 40],
+[23, 9, 1, 40],
+[36, 13, 1, 40],
+[2, 1, 1, 40],
+[2, 1, 1, 40],
+[18, 6, 1, 40],
+[2, 1, 1, 40],
+[6, 12, 1, 40],
+[8, 14, 1, 40],
+[44, 11, 1, 40],
+[2, 1, 1, 40],
+[28, 7, 1, 40]]
 
 row_names = ["frangieh/co_culture/gene",
              "frangieh/control/gene",
@@ -46,4 +55,5 @@ row_names = ["frangieh/co_culture/gene",
 
 col_names = ["schraivogel_method",
              "seurat_de",
-             "liscovitch_method"]
+             "liscovitch_method",
+             "mimosca"]
