195943f73f5abcdfb9b75ffa2a266ecab77052f2
Tim <tbarry2@andrew.cmu.edu>
2022-06-07 17:37:47 -0400

    add command line arg grna modality

--- bash_scripts/run_undercover_gRNA_pipeline.sh
+++ bash_scripts/run_undercover_gRNA_pipeline.sh
@@ -8,9 +8,10 @@ nextflow pull https://github.com/Katsevich-Lab/undercover-gRNA-pipeline
 nextflow run undercover-gRNA-pipeline -r main \
 nextflow run $LOCAL_CODE_DIR"/undercover-gRNA-pipeline/main.nf" \
  --data_method_pair_file $LOCAL_CODE_DIR"/sceptre2-manuscript/param_files/test_file_2.groovy" \
- --machine_name $MACHINE_NAME \
  --result_dir $LOCAL_SCEPTRE2_DATA_DIR"results/undercover_grna_analysis" \
- --time $curr_time \
+ --result_file_name "trial.rds" \
  --one_neg_control "TRUE" \
  --max_retries 4 \
- --result_file_name "trial.rds"
+ --grna_modality "assignment" \
+ --machine_name $MACHINE_NAME \
+ --time $curr_time
