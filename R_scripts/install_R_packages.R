3bf6e2497e3b82631af7683c902b7083efce1dd2
ekatsevi <katsevich.gene@gmail.com>
2022-06-08 18:03:12 -0400

    Added script to install R packages

---
+++ R_scripts/install_R_packages.R
@@ -0,0 +1,17 @@
+# CRAN
+install.packages("readr")
+install.packages("dplyr")
+install.packages("readxl")
+install.packages("Seurat")
+install.packages("R.utils")
+install.packages("purrr")
+install.packages("RCurl")
+
+# GitHub
+devtools::install_github("Timothy-Barry/ondisc")
+devtools::install_github('satijalab/seurat-data')
+devtools::install_github("Katsevich-Lab/lowmoi")
+
+# Bioconductor
+BiocManager::install("GenomicRanges")
+
