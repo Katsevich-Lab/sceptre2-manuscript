61822773b4be8a844c4dad132ec8589c2d66e830
ekatsevi <katsevich.gene@gmail.com>
2022-06-08 15:37:00 -0400

    Updated setup.sh

--- bash_scripts/setup.sh
+++ bash_scripts/setup.sh
@@ -56,29 +56,36 @@ sceptre2_data_dir=$sceptre2_dir"data/"
 
 rm -rf $sceptre2_data_dir
 mkdir -p $sceptre2_data_dir"schraivogel/ground_truth_tapseq/gene" \
- $sceptre2_data_dir"schraivogel/ground_truth_tapseq/grna" \
+ $sceptre2_data_dir"schraivogel/ground_truth_tapseq/grna_expression" \
+  $sceptre2_data_dir"schraivogel/ground_truth_tapseq/grna_assignment" \
  $sceptre2_data_dir"schraivogel/ground_truth_perturbseq/gene" \
- $sceptre2_data_dir"schraivogel/ground_truth_perturbseq/grna" \
+ $sceptre2_data_dir"schraivogel/ground_truth_perturbseq/grna_expression" \
+  $sceptre2_data_dir"schraivogel/ground_truth_perturbseq/grna_assignment" \
  $sceptre2_data_dir"schraivogel/enhancer_screen_chr8/gene" \
- $sceptre2_data_dir"schraivogel/enhancer_screen_chr8/grna" \
+ $sceptre2_data_dir"schraivogel/enhancer_screen_chr8/grna_expression" \
+  $sceptre2_data_dir"schraivogel/enhancer_screen_chr8/grna_assignment" \
  $sceptre2_data_dir"schraivogel/enhancer_screen_chr11/gene" \
- $sceptre2_data_dir"schraivogel/enhancer_screen_chr11/grna" \
+ $sceptre2_data_dir"schraivogel/enhancer_screen_chr11/grna_expression" \
+  $sceptre2_data_dir"schraivogel/enhancer_screen_chr11/grna_assignment" \
  $sceptre2_data_dir"papalexi/eccite_screen/gene" \
- $sceptre2_data_dir"papalexi/eccite_screen/grna" \
+ $sceptre2_data_dir"papalexi/eccite_screen/grna_expression" \
+  $sceptre2_data_dir"papalexi/eccite_screen/grna_assignment" \
  $sceptre2_data_dir"papalexi/eccite_screen/protein" \
  $sceptre2_data_dir"liscovitch/experiment_small/chromatin" \
- $sceptre2_data_dir"liscovitch/experiment_small/grna" \
- $sceptre2_data_dir"liscovitch/experiment_big/grna" \
+ $sceptre2_data_dir"liscovitch/experiment_small/grna_expression" \
+  $sceptre2_data_dir"liscovitch/experiment_small/grna_assignment" \
+ $sceptre2_data_dir"liscovitch/experiment_big/grna_expression" \
+  $sceptre2_data_dir"liscovitch/experiment_big/grna_assignment" \
  $sceptre2_data_dir"liscovitch/experiment_big/chromatin" \
  $sceptre2_data_dir"frangieh/co_culture/gene" \
  $sceptre2_data_dir"frangieh/co_culture/protein" \
- $sceptre2_data_dir"frangieh/co_culture/grna" \
+ $sceptre2_data_dir"frangieh/co_culture/grna_assignment" \
  $sceptre2_data_dir"frangieh/control/gene" \
  $sceptre2_data_dir"frangieh/control/protein" \
- $sceptre2_data_dir"frangieh/control/grna" \
+ $sceptre2_data_dir"frangieh/control/grna_assignment" \
  $sceptre2_data_dir"frangieh/ifn_gamma/gene" \
  $sceptre2_data_dir"frangieh/ifn_gamma/protein" \
- $sceptre2_data_dir"frangieh/ifn_gamma/grna" \
+ $sceptre2_data_dir"frangieh/ifn_gamma/grna_assignment" \
  $sceptre2_data_dir"simulated/experiment_1/gene" \
  $sceptre2_data_dir"simulated/experiment_1/grna_expression" \
  $sceptre2_data_dir"simulated/experiment_1/grna_assignment" \
