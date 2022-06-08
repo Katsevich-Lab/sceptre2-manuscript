03ad6cbd7b245a42823e6edf6847c33faad42e7d
ekatsevi <katsevich.gene@gmail.com>
2022-06-08 14:51:52 -0400

    Added sourcing of .research_config at the top of import_all.sh

--- bash_scripts/import_all.sh
+++ bash_scripts/import_all.sh
@@ -1,3 +1,4 @@
+source ~/.research_config
 qsub -q short.q -l m_mem_free=16G $LOCAL_CODE_DIR"/import-schraivogel-2020/run_all.sh"
 qsub -q short.q -l m_mem_free=16G $LOCAL_CODE_DIR"/import-papalexi-2021/run_all.sh"
 qsub -q short.q -l m_mem_free=16G $LOCAL_CODE_DIR"/import-liscovitch-2021/run_all.sh"
