edd25d3d3500b3a950c22af75765f887287a31d5
ekatsevi <katsevich.gene@gmail.com>
2022-06-08 15:37:59 -0400

    moved creation of simulated data to step 3

--- bash_scripts/run_all.sh
+++ bash_scripts/run_all.sh
@@ -4,9 +4,9 @@
 bash setup.sh
 # 2. create the symbolic links within the offsite directory structure
 bash create_sym_links.sh
-# 3. cell-specific QC on Frangieh and Liscovitch data
-Rscript ../R_scripts/cell_qc.R
-# 4. feature-specific QC on all data
-Rscript ../R_scripts/feature_qc.R
-# 5. create the synthetic data
+# 3. create the synthetic data
 Rscript ../R_scripts/create_synthetic_data.R
+# 4. cell-specific QC on Frangieh and Liscovitch data
+Rscript ../R_scripts/cell_qc.R
+# 5. feature-specific QC on all data
+Rscript ../R_scripts/feature_qc.R
