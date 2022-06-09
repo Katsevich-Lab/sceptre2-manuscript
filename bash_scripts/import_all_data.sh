ce859a6a207ec3ce33f122be8365a81d59585427
ekatsevi <katsevich.gene@gmail.com>
2022-06-09 13:18:00 -0400

    Added Frangieh import to import_all_data.sh

--- bash_scripts/import_all_data.sh
+++ bash_scripts/import_all_data.sh
@@ -8,3 +8,6 @@ qsub -q short.q -l m_mem_free=16G $LOCAL_CODE_DIR"/import-papalexi-2021/run_all.
 
 cd $LOCAL_CODE_DIR"/import-liscovitch-2021"
 qsub -q short.q -l m_mem_free=16G $LOCAL_CODE_DIR"/import-liscovitch-2021/run_all.sh"
+
+cd $LOCAL_CODE_DIR"/import-frangieh-2021"
+qsub -q short.q -l m_mem_free=250G $LOCAL_CODE_DIR"/import-frangieh-2021/run_all.sh"
