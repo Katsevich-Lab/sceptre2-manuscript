334c0c437e3d8af51800921c93ab5f01448e0bc2
ekatsevi <katsevich.gene@gmail.com>
2022-06-27 18:27:36 -0400

    Made progress on perturbation propensity analysis

--- R_scripts/perturbation_propensity.R
+++ R_scripts/perturbation_propensity.R
@@ -2,15 +2,13 @@ data_dir <- paste0(.get_config_path("LOCAL_SCEPTRE2_DATA_DIR"), "data/")
 papers <- list.files(data_dir)
 
 # loop over papers
-results <- list()
+results <- tibble(paper = character(), dataset = character(), ntc = character(),
+                  pvalue = numeric(), zvalue = numeric(), effect_size = numeric())
 for(paper in papers){
-  results$paper <- list()
   paper_dir <- paste0(data_dir, paper, "/")
   datasets <- list.files(paper_dir)
   # loop over datasets within a paper
   for (dataset in datasets) {
-    results$paper$dataset <- list()
-
     # get gRNA ODM
     grna_fp <- paste0(paper, "/", dataset, "/grna_assignment")
     grna_odm <- lowmoi::load_dataset_modality(data_fp = gRNA_fp)
@@ -20,38 +18,41 @@ for(paper in papers){
     gene_odm <- lowmoi::load_dataset_modality(data_fp = gene_fp)
 
     # get list of NTC gRNAs
-    ntcs <- gRNA_odm |>
+    ntcs <- grna_odm |>
       get_feature_covariates() |>
       filter(target == "non-targeting") |>
       rownames() |>
       unique()
 
     # get the gRNA assigned to each cell
-    assigned_grnas <- sapply(1:ncol(gRNA_odm), function(col)(gRNA_odm[[,col]] |> as.logical() |> which()))
-    grnas <- gRNA_odm |> get_feature_ids()
-    assigned_grnas <- lapply(assigned_grnas, function(grna_idx)(if(length(grna_idx) == 1) grnas[grna_idx] else ""))
-
-
-    # assigned_grnas <- gRNA_odm |>
-    #   get_cell_covariates() |>
-    #   pull(assigned_gRNA)
-
+    assigned_grnas_df <- grna_odm |>
+      get_cell_covariates() |>
+      select(assigned_gRNA)
 
     # get a logical vector of whether a cell was assigned an NTC
-    ntc_cells <- assigned_grnas |>
+    ntc_cells <- assigned_grnas_df |>
       mutate(ntc = assigned_gRNA %in% ntcs) |>
       pull(ntc)
 
     # restrict assigned gRNAs to cells with an NTC
-    assigned_grnas <- assigned_grnas[ntc_cells]
+    assigned_grnas <- assigned_grnas_df[ntc_cells,,drop=F] |> pull(assigned_gRNA)
 
     # extract cell covariates from gene ODM, and restrict attention to cells with NTC
     cell_covariates <- gene_odm[,ntc_cells] |> get_cell_covariates()
 
+    # append assigned_grnas column to cell_covariates
+    df <- cell_covariates |>
+      mutate(assigned_grna = assigned_grnas)
+
     # carry out the regression for each NTC
     for(ntc in ntcs){
-      perturbation_indicator <- assigned_grnas == ntc
-      glm_fit <- glm(perturbation_indicator ~ cell_covariates, family = "binomial")
+      df_ntc <- df |>
+        mutate(perturbation_indicator = as.numeric(assigned_grna == ntc)) |>
+        select(-assigned_grna, -condition)
+      glm_fit <- glm(perturbation_indicator ~ ., family = "binomial", data = df_ntc)
+      fit_info <- coef(summary(glm_fit))
+      results |> add_row(paper = paper, dataset = dataset, ntc = ntc,
+                         pvalue = numeric(), zvalue = numeric(), effect_size = numeric())
     }
   }
 }
