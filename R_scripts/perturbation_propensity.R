3e3412630b2dc0194a9e95f4339ef1d766f1515d
ekatsevi <katsevich.gene@gmail.com>
2022-06-28 12:39:40 -0400

    Completed first  draft of perturbation propensity analysis

--- R_scripts/perturbation_propensity.R
+++ R_scripts/perturbation_propensity.R
@@ -1,26 +1,42 @@
 data_dir <- paste0(.get_config_path("LOCAL_SCEPTRE2_DATA_DIR"), "data/")
 papers <- list.files(data_dir)
 
+# set up results tibble
+results <- tibble(
+  paper = character(),
+  dataset = character(),
+  ntc = character(),
+  covariate = character(),
+  estimate = numeric(),
+  std_error = numeric(),
+  zvalue = numeric(),
+  pvalue = numeric()
+)
+
 # loop over papers
-results <- tibble(paper = character(), dataset = character(), ntc = character(),
-                  pvalue = numeric(), zvalue = numeric(), effect_size = numeric())
-for(paper in papers){
+for (paper in papers) {
   paper_dir <- paste0(data_dir, paper, "/")
   datasets <- list.files(paper_dir)
   # loop over datasets within a paper
   for (dataset in datasets) {
+    cat(sprintf("Working on the %s dataset from %s...\n", dataset, paper))
+
     # get gRNA ODM
     grna_fp <- paste0(paper, "/", dataset, "/grna_assignment")
-    grna_odm <- lowmoi::load_dataset_modality(data_fp = gRNA_fp)
+    grna_odm <- lowmoi::load_dataset_modality(data_fp = grna_fp)
 
-    # get gene ODM
-    gene_fp <- paste0(paper, "/", dataset, "/gene")
-    gene_odm <- lowmoi::load_dataset_modality(data_fp = gene_fp)
+    # get response ODM
+    if (paper == "liscovitch") {
+      response_fp <- paste0(paper, "/", dataset, "/chromatin")
+    } else {
+      response_fp <- paste0(paper, "/", dataset, "/gene")
+    }
+    response_odm <- lowmoi::load_dataset_modality(data_fp = response_fp)
 
     # get list of NTC gRNAs
     ntcs <- grna_odm |>
       get_feature_covariates() |>
-      filter(target == "non-targeting") |>
+      filter(target %in% c("non-targeting", "NT")) |>
       rownames() |>
       unique()
 
@@ -35,24 +51,50 @@ for(paper in papers){
       pull(ntc)
 
     # restrict assigned gRNAs to cells with an NTC
-    assigned_grnas <- assigned_grnas_df[ntc_cells,,drop=F] |> pull(assigned_gRNA)
+    assigned_grnas <- assigned_grnas_df[ntc_cells, , drop = F] |> pull(assigned_gRNA)
 
     # extract cell covariates from gene ODM, and restrict attention to cells with NTC
-    cell_covariates <- gene_odm[,ntc_cells] |> get_cell_covariates()
+    cell_covariates <- response_odm[, ntc_cells] |> get_cell_covariates()
 
     # append assigned_grnas column to cell_covariates
     df <- cell_covariates |>
       mutate(assigned_grna = assigned_grnas)
 
     # carry out the regression for each NTC
-    for(ntc in ntcs){
+    for (ntc in ntcs) {
+      # create perturbation indicator based on given NTC
       df_ntc <- df |>
-        mutate(perturbation_indicator = as.numeric(assigned_grna == ntc)) |>
-        select(-assigned_grna, -condition)
+        mutate(perturbation_indicator = as.numeric(assigned_grna == ntc))
+
+      # remove unnecessary columns from cell covariates
+      df_ntc <- df_ntc |> select(-assigned_grna)
+      if (paper == "frangieh") {
+        df_ntc <- df_ntc |> select(-condition)
+      }
+      if (paper == "papalexi") {
+        df_ntc <- df_ntc |> select(-guide_ID)
+      }
+
+      # fit GLM
       glm_fit <- glm(perturbation_indicator ~ ., family = "binomial", data = df_ntc)
-      fit_info <- coef(summary(glm_fit))
-      results |> add_row(paper = paper, dataset = dataset, ntc = ntc,
-                         pvalue = numeric(), zvalue = numeric(), effect_size = numeric())
+
+      # extract fit information
+      fit_info <- coef(summary(glm_fit)) |>
+        as.data.frame() |>
+        rownames_to_column(var = "covariate") |>
+        filter(covariate != "(Intercept)") |>
+        rename(estimate = Estimate, std_error = `Std. Error`, zvalue = `z value`, pvalue = `Pr(>|z|)`) |>
+        mutate(paper = paper, dataset = dataset, ntc = ntc) |>
+        select(paper, dataset, ntc, covariate, estimate, std_error, zvalue, pvalue)
+
+      # add to results data frame
+      results <- results |> bind_rows(fit_info)
     }
   }
 }
+
+# save results to disk
+results_dir <- paste0(.get_config_path("LOCAL_SCEPTRE2_DATA_DIR"),
+                      "results/perturbation_propensity_analysis")
+dir.create(results_dir)
+saveRDS(results, paste0(results_dir, "/results.rds"))
