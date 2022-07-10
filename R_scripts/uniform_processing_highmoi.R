3a60fc150477ab5c67b78f9f2e5f08c45159d835
Tim <tbarry2@andrew.cmu.edu>
2022-07-10 16:03:28 -0400

    tighten feature qc for high moi uniform processing

--- R_scripts/uniform_processing_highmoi.R
+++ R_scripts/uniform_processing_highmoi.R
@@ -2,7 +2,7 @@ library(ondisc)
 sceptre2_data_dir <- paste0(.get_config_path("LOCAL_SCEPTRE2_DATA_DIR"), "data/")
 papers <- "gasperini"
 
-FRAC_EXPRESSED_TRHESH <- 0.005
+FRAC_EXPRESSED_TRHESH <- 0.01
 N_CELLS_PER_GRNA_THRESH <- 10
 
 for (paper in papers) {
