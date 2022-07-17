94b83f0fffea5b3bf8b4979907e1874fe6f57034
Tim <tbarry2@andrew.cmu.edu>
2022-07-17 16:33:19 -0400

    update run_with_resume script for group=2

--- pipeline_launch_scripts/undercover/grp_2/run_with_resume.sh
+++ pipeline_launch_scripts/undercover/grp_2/run_with_resume.sh
@@ -1,1 +1,13 @@
-nextflow run undercover-grna-pipeline -r main -resume irreverent_hamilton
+nextflow run undercover-grna-pipeline -r main \
+ --data_method_pair_file /home/stat/timbar/research_code/sceptre2-manuscript/param_files/data_method_pair_file.groovy \
+ --result_dir /home/stat/timbar/data/projects/sceptre2/results/undercover_grna_analysis \
+ --result_file_name undercover_result_grp_size_2.rds \
+ --grna_modality assignment \
+ --group_size 2 \
+ --is_group_size_frac false \
+ --partition_count 1 \
+ --is_partition_count_frac true \
+ --machine_name hpcc \
+ --time 07160511 \
+ -profile standard \
+ -resume
