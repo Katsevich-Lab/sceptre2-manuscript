e8cd388623cae9fcd8b20a478e8b0d4c66d44345
Tim <tbarry2@andrew.cmu.edu>
2022-08-05 10:58:38 -0400

    update aug pres

--- writeups/digging_into_undercover/test_permutation.R
+++ writeups/digging_into_undercover/test_permutation.R
@@ -14,10 +14,14 @@ grna_feature_covariates[undercover_ntc_name, "target"] <- "undercover"
 grna_odm_swapped <- grna_odm |> mutate_feature_covariates(target = grna_feature_covariates$target)
 pairs_df <- data.frame(grna_group = "undercover", response_id = response_id)
 
-perm_res <- permutation_test(response_odm = response_odm,
-                             grna_odm = grna_odm_swapped,
-                             response_grna_group_pairs = pairs_df,
-                             return_permuted_test_stats = TRUE,
-                             test_stat = "log_fold_change")
+perm_res_1 <- permutation_test(response_odm = response_odm,
+                               grna_odm = grna_odm_swapped,
+                               response_grna_group_pairs = pairs_df,
+                               return_permuted_test_stats = TRUE,
+                               test_stat = "log_fold_change")
+
+# perm_res_2 <- permutation_test_pl
+
+
 perm_res[,1:5]
 perm_res |> dplyr::select(resample_1:resample_1000) |> as.numeric() |> hist()
