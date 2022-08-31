b2d7ee184976582ce5b44d53f94ad9173b53792b
Tim <tbarry2@andrew.cmu.edu>
2022-08-31 13:09:24 -0400

    scale up debug pipeline

--- pipeline_launch_scripts/debug/debug.sh
+++ pipeline_launch_scripts/debug/debug.sh
@@ -13,8 +13,8 @@ nextflow run Katsevich-Lab/undercover-grna-pipeline -r main \
  --group_size "1" \
  --is_group_size_frac "false" \
  --partition_count 1 \
- --is_partition_count_frac "false" \
+ --is_partition_count_frac "true" \
  --machine_name $MACHINE_NAME \
  --time $curr_time \
- --genes_to_subsample "5" \
+ --genes_to_subsample "500" \
  -profile standard
