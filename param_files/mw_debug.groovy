0efc2d5c6365d1d02d21c836af1354dd16fc5b66
Tim <tbarry2@andrew.cmu.edu>
2022-10-25 18:48:39 -0400

    update mw param flie

--- param_files/mw_debug.groovy
+++ param_files/mw_debug.groovy
@@ -1,5 +1,12 @@
 // FIRST, define the dataset-method pairs to analyze in a map
-data_method_pairs = ["papalexi/eccite_screen/gene": ["mann_whitney_perm"]]
+data_method_pairs = ["frangieh/co_culture/gene": ["mann_whitney_perm_mimosca"],
+                     "frangieh/control/gene": ["mann_whitney_perm_mimosca"],
+                     "frangieh/ifn_gamma/gene": ["mann_whitney_perm_mimosca"],
+                     "papalexi/eccite_screen/gene": ["mann_whitney_perm_mimosca"],
+                     "papalexi/eccite_screen/protein": ["mann_whitney_perm_mimosca"],
+                     "schraivogel/ground_truth_perturbseq/gene": ["mann_whitney_perm_mimosca"],
+                     "schraivogel/ground_truth_tapseq/gene": ["mann_whitney_perm_mimosca"],
+                     ]
 
 
 // FIRST, define the row and column names of the below matrices and vectors
@@ -15,43 +22,43 @@ row_names = ["frangieh/co_culture/gene",
              "schraivogel/ground_truth_perturbseq/gene",
              "schraivogel/ground_truth_tapseq/gene",
              "simulated/experiment_1/gene"]
-col_names = ["mann_whitney_perm"]
+col_names = ["mann_whitney_perm_mimosca"]
 
 // SECOND, define a matrix indicating the amount of RAM to request for each dataset-method pair
 data_method_ram_matrix = [
-[5], // frangieh/co_culture/gene
-[5], // frangieh/control/gene
-[5], // frangieh/ifn_gamma/gene
+[13], // frangieh/co_culture/gene
+[45], // frangieh/control/gene
+[13], // frangieh/ifn_gamma/gene
 [1], // liscovitch/experiment_big/chromatin
 [1], // liscovitch/experiment_small/chromatin
-[5], // papalexi/eccite_screen/gene
-[5], // papalexi/eccite_screen/protein
-[5], // schraivogel/enhancer_screen_chr11/gene
-[5], // schraivogel/enhancer_screen_chr8/gene
-[5], // schraivogel/ground_truth_perturbseq/gene
-[5], // schraivogel/ground_truth_tapseq/gene
-[5] // simulated/experiment_1/gene
+[6], // papalexi/eccite_screen/gene
+[1], // papalexi/eccite_screen/protein
+[12], // schraivogel/enhancer_screen_chr11/gene
+[14], // schraivogel/enhancer_screen_chr8/gene
+[11], // schraivogel/ground_truth_perturbseq/gene
+[1], // schraivogel/ground_truth_tapseq/gene
+[7] // simulated/experiment_1/gene
 ]
 // mann_whitney_perm
 
 // THIRD, define a matrix indicating the queue in which to put a given dataset-method pair process
 data_method_queue_matrix = [
-["all.q"],  // frangieh/co_culture/gene
-["all.q"], // frangieh/control/gene
-["all.q"],  // frangieh/ifn_gamma/gene
+["short.q"],  // frangieh/co_culture/gene
+["short.q"], // frangieh/control/gene
+["short.q"],  // frangieh/ifn_gamma/gene
 ["short.q"], // liscovitch/experiment_big/chromatin
 ["short.q"], // liscovitch/experiment_small/chromatin
-["all.q"],  // papalexi/eccite_screen/gene
+["short.q"],  // papalexi/eccite_screen/gene
 ["short.q"], // papalexi/eccite_screen/protein
-["all.q"], // schraivogel/enhancer_screen_chr11/gene
-["all.q"],  // schraivogel/enhancer_screen_chr8/gene
-["all.q"], // schraivogel/ground_truth_perturbseq/gene
-["all.q"], // schraivogel/ground_truth_tapseq/gene
-["all.q"] // simulated/experiment_1/gene
+["short.q"], // schraivogel/enhancer_screen_chr11/gene
+["short.q"],  // schraivogel/enhancer_screen_chr8/gene
+["short.q"], // schraivogel/ground_truth_perturbseq/gene
+["short.q"], // schraivogel/ground_truth_tapseq/gene
+["short.q"] // simulated/experiment_1/gene
 ]
 // mann_whitney_perm
 
 // FOURTH, define an ordered list of optional arguments to each of the methods (Should be strings of the form "arg1=value1:arg2=value2:arg3=value3")
 optional_args = [
-  "B=100000:progress=FALSE" // mann_whitney_perm
+  "B=1" // mann_whitney_perm
 ]
--- pipeline_launch_scripts/debug/debug_mw.sh
+++ pipeline_launch_scripts/debug/debug_mw.sh
@@ -8,7 +8,7 @@ curr_time=$(date '+%m%d%H%M')
 nextflow run Katsevich-Lab/undercover-grna-pipeline -r main \
  --data_method_pair_file $LOCAL_CODE_DIR"/sceptre2-manuscript/param_files/mw_debug.groovy" \
  --result_dir $LOCAL_SCEPTRE2_DATA_DIR"results/undercover_grna_analysis" \
- --result_file_name "mw_debug.rds" \
+ --result_file_name "mw_mimosca_1.rds" \
  --grna_modality "assignment" \
  --group_size "1" \
  --is_group_size_frac "false" \
@@ -16,5 +16,4 @@ nextflow run Katsevich-Lab/undercover-grna-pipeline -r main \
  --is_partition_count_frac "true" \
  --machine_name $MACHINE_NAME \
  --time $curr_time \
- --pair_restriction $LOCAL_SCEPTRE2_DATA_DIR"results/undercover_grna_analysis/mw_debug_pairs.rds" \
  -profile standard
