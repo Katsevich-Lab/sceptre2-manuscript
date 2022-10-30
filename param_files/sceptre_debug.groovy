2646221cb45d5a730d95dacaf5f0c40ec297f899
Tim <tbarry2@andrew.cmu.edu>
2022-10-30 19:45:05 -0400

    update sceptre_debug.groovy; run on all real datasets

--- param_files/sceptre_debug.groovy
+++ param_files/sceptre_debug.groovy
@@ -3,6 +3,11 @@ data_method_pairs = ["frangieh/co_culture/gene": ["sceptre"],
                      "frangieh/control/gene": ["sceptre"],
                      "frangieh/ifn_gamma/gene": ["sceptre"],
                      "papalexi/eccite_screen/gene": ["sceptre"]
+                     "papalexi/eccite_screen/protein": ["sceptre"],
+                     "schraivogel/enhancer_screen_chr11/gene": ["sceptre"],
+                     "schraivogel/enhancer_screen_chr8/gene": ["sceptre"],
+                     "schraivogel/ground_truth_perturbseq/gene": ["sceptre"],
+                     "schraivogel/ground_truth_tapseq/gene": ["sceptre"]
                      ]
 
 
