b6db35373254e61647be623207681fee7388210c
Tim <tbarry2@andrew.cmu.edu>
2022-11-15 23:14:54 -0500

    prepare launch of big undercover pipeline (grp size = 1)

--- pipeline_launch_scripts/undercover/grp_size_1/grp_size_1.sh
+++ pipeline_launch_scripts/undercover/grp_size_1/grp_size_1.sh
@@ -13,7 +13,7 @@ nextflow run Katsevich-Lab/undercover-grna-pipeline -r main \
  --group_size "1" \
  --is_group_size_frac "false" \
  --partition_count "1" \
- --is_partition_count_frac "false" \
+ --is_partition_count_frac "true" \
  --time $curr_time \
  -profile standard \
  -with-trace \
