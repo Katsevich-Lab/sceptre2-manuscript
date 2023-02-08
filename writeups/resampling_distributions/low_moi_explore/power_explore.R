abd54090ae93b3fe927f0d27e645c0de53bf85c9
Ziang Niu <ziangniu6@gmail.com>
2023-02-08 10:09:34 -0500

    Added power results

--- writeups/resampling_distributions/low_moi_explore/power_explore.R
+++ writeups/resampling_distributions/low_moi_explore/power_explore.R
@@ -28,11 +28,12 @@ resampling_dists <- readRDS(paste0(sceptre2_results_dir, "resampling_distributio
 # k-s test using fitted distribution
 samples_list <- c(round(seq(5e4, 1e3, length.out = 10)))
 power_mat <- list()
+power_list <- list()
 for (i in 1:length(samples_list)){
-  power_mat[[i]] <- matrix(0, nrow = 1000, ncol = 330)
   no_resamp <- samples_list[i]
   q <- 0.96
-  for (r in 73:100) {
+  for (r in 98:100) {
+    power_mat[[r]] <- matrix(0, nrow = 330, ncol = 10)
     for (l in 1:330){
       # set.seed
       set.seed(r)
@@ -54,29 +55,35 @@ for (i in 1:length(samples_list)){
         xi_mom <- (1 - (mean(trun_data) - u_pos)^2/var(trun_data))/2
         sig_mom <- (mean(trun_data) - u_pos)*(1 - xi_mom)
         
-        sim_data <- rgpd(1e6, loc = u_pos, scale = sig_mom, shape = xi_mom)
+        # first fit ks.test with estimated parameter; if there is Na, switch to simulation
+        test_ks <- ks.test(trun_data, "pgpd", u_pos, sig_mom, xi_mom)
+        print(is.nan(test_ks$p.value))
+        print(test_ks$p.value)
+        if(is.nan(test_ks$p.value)){
+          sim_data <- rgpd(1e6, loc = u_pos, scale = sig_mom, shape = xi_mom)
+          # k-s test
+          test_ks <- ks.test(trun_data, sim_data)
+        }
         
-        # k-s test
-        test_ks <- ks.test(trun_data, sim_data)
         
         # store p_value and test statistic
         if(tail == "right"){
-          power_mat[[i]][6, r] <- u_pos
-          power_mat[[i]][7, r] <- sig_mom
-          power_mat[[i]][8, r] <- xi_mom
-          power_mat[[i]][9,r] <- test_ks$statistic
-          power_mat[[i]][10,r] <- test_ks$p.value
+          power_mat[[r]][l, 6] <- as.numeric(u_pos)
+          power_mat[[r]][l, 7] <- sig_mom
+          power_mat[[r]][l, 8] <- xi_mom
+          power_mat[[r]][l, 9] <- test_ks$statistic
+          power_mat[[r]][l, 10] <- test_ks$p.value
         }else{
-          power_mat[[i]][1, r] <- u_pos
-          power_mat[[i]][2, r] <- sig_mom
-          power_mat[[i]][3, r] <- xi_mom
-          power_mat[[i]][4,r] <- test_ks$statistic
-          power_mat[[i]][5,r] <- test_ks$p.value
+          power_mat[[r]][l, 1] <- as.numeric(u_pos)
+          power_mat[[r]][l, 2] <- sig_mom
+          power_mat[[r]][l, 3] <- xi_mom
+          power_mat[[r]][l, 4] <- test_ks$statistic
+          power_mat[[r]][l, 5] <- test_ks$p.value
         }
       }   
-      print(power_mat[[i]][1:4,r])
     }
     print(r)
   }
+  power_list[[i]] <- power_mat
 }
-
+write.csv(power_list, "power_list.csv")
