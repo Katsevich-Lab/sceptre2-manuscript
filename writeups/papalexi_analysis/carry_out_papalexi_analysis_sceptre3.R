d056722dcd64b481b28af9691f6449510a58ad60
Kmason23 <Kaishu@wharton.upenn.edu>
2023-03-01 09:25:06 -0500

    used tim's papalexi analysis

--- writeups/papalexi_analysis/carry_out_papalexi_analysis_sceptre3.R
+++ writeups/papalexi_analysis/carry_out_papalexi_analysis_sceptre3.R
@@ -42,64 +42,13 @@ covariate_data_frame <- gene_covariate_matrix
 grna_group_data_frame <- grna_groups
 formula_object <- gene_formula
 calibration_check <- FALSE
-<<<<<<< HEAD
-unique_grna = unique(grna_groups$grna_group)
-response_grna_group_pairs_1 <- expand.grid(response_id = 'CD274',
-                                         grna_group = unique_grna[-which(unique_grna == 'non-targeting')]) # an example set of pairs
-response_grna_group_pairs_2 <- expand.grid(response_id = rownames(response_matrix),
-                                           grna_group = "CUL3") # an example set of pairs
-response_grna_group_pairs = rbind(response_grna_group_pairs_1,response_grna_group_pairs_2)
-=======
-response_grna_group_pairs <- data.frame(response_id = feature_ids[13565:13575], grna_group = "CUL3") # data.frame(response_id = "ZNF546", grna_group = "CUL3") # an example set of pairs
->>>>>>> 2acdd2c29b4c84cc510285df6fa6ae2ac1496c18
+response_grna_group_pairs <- data.frame(response_id = rownames(gene_expression_matrix)[13565:13575], grna_group = "CUL3") # data.frame(response_id = "ZNF546", grna_group = "CUL3") # an example set of pairs
 test_stat <- "full"
 return_resampling_dist <- FALSE
 adaptive_permutation_test <- TRUE
 fit_skew_normal <- FALSE
 
 result_gene <- run_sceptre_lowmoi(response_matrix,
-<<<<<<< HEAD
-                             grna_matrix,
-                             covariate_data_frame,
-                             grna_group_data_frame,
-                             formula_object,
-                             calibration_check,
-                             response_grna_group_pairs_2,
-                             test_stat,
-                             return_resampling_dist,
-                             adaptive_permutation_test,
-                             fit_skew_normal)
-
-=======
-                                  grna_matrix,
-                                  covariate_data_frame,
-                                  grna_group_data_frame,
-                                  formula_object,
-                                  calibration_check,
-                                  response_grna_group_pairs,
-                                  test_stat,
-                                  return_resampling_dist,
-                                  adaptive_permutation_test,
-                                  fit_skew_normal)
->>>>>>> 2acdd2c29b4c84cc510285df6fa6ae2ac1496c18
-
-##########################################
-# SET ARGS FOR PROTEIN EXPRESSION ANALYSIS
-##########################################
-response_matrix <- as.matrix(protein_matrix)
-grna_matrix <- grna_matrix
-covariate_data_frame <- protein_covariate_matrix
-grna_group_data_frame <- grna_groups
-formula_object <- protein_formula
-calibration_check <- FALSE
-response_grna_group_pairs <- expand.grid(response_id = rownames(response_matrix),
-                                         grna_group = unique(grna_groups$grna_group)[1:2]) # an example set of pairs
-test_stat <- "full"
-return_resampling_dist <- FALSE
-adaptive_permutation_test <- TRUE
-fit_skew_normal <- FALSE
-
-result_protein <- run_sceptre_lowmoi(response_matrix,
                                   grna_matrix,
                                   covariate_data_frame,
                                   grna_group_data_frame,
