9b6e1ead8238878fb4a60e446c3d25f5da4619e3
Tim <tbarry2@andrew.cmu.edu>
2023-03-13 14:03:46 -0400

    export plot neg cntrl funct

--- writeups/frangieh_analysis/approximate_undercover_analysis.R
+++ writeups/frangieh_analysis/approximate_undercover_analysis.R
@@ -1,15 +1,15 @@
 # This script uses sceptre3 to run an approximate undercover analysis on the 
 LOCAL_SCEPTRE2_DATA_DIR <-.get_config_path("LOCAL_SCEPTRE2_DATA_DIR")
-papalexi_dir <- paste0(LOCAL_SCEPTRE2_DATA_DIR, "data/papalexi/eccite_screen/")
+ifn_gamma_dir <- paste0(LOCAL_SCEPTRE2_DATA_DIR, "data/frangieh/ifn_gamma/")
 
 # gene info
-gene_odm_fp <- paste0(papalexi_dir, "gene/matrix.odm")
+gene_odm_fp <- paste0(ifn_gamma_dir, "gene/matrix.odm")
 
 # grna info
-grna_odm_fp <- paste0(papalexi_dir, "grna_expression/matrix.odm")
+grna_odm_fp <- paste0(ifn_gamma_dir, "grna_assignment/matrix.odm")
 
 # mm odm metadata fp
-mm_metadata_fp <- paste0(papalexi_dir, "multimodal_metadata.rds")
+mm_metadata_fp <- paste0(ifn_gamma_dir, "multimodal_metadata.rds")
 
 # construct mm odm
 mm_odm <- ondisc::read_multimodal_odm(odm_fps = c(gene_odm_fp, grna_odm_fp),
@@ -21,7 +21,7 @@ response_matrix <- gene_odm[[seq(1, nrow(gene_odm)),]]
 rownames(response_matrix) <- ondisc::get_feature_ids(gene_odm)
 
 # get the in-memory grna matrix
-grna_odm <- mm_odm |> ondisc::get_modality("grna_expression")
+grna_odm <- mm_odm |> ondisc::get_modality("grna_assignment")
 grna_matrix <- grna_odm[[seq(1, nrow(grna_odm)),]]
 
 # covariate matrix
@@ -30,9 +30,12 @@ covariate_data_frame <- mm_odm |> ondisc::get_cell_covariates()
 # grna group data frame
 grna_group_data_frame <- data.frame(grna_id = rownames(grna_odm@feature_covariates),
                                     grna_group = grna_odm@feature_covariates$target)
+n_ntc <- grna_group_data_frame |>
+  dplyr::filter(grna_group == "non-targeting") |>
+  nrow()
 
 # set formulas, grna group target name
-gene_formula <- ~ log(gene_n_umis) + log(gene_n_nonzero) + bio_rep + phase + p_mito
+gene_formula <- formula(~log(gene_n_umis) + log(gene_n_nonzero) + phase) #  mm_odm@global_misc$formula
 
 undercover_result <- sceptre3::run_sceptre_lowmoi(response_matrix = response_matrix,
                                                   grna_matrix = grna_matrix,
@@ -48,7 +51,10 @@ undercover_result <- sceptre3::run_sceptre_lowmoi(response_matrix = response_mat
                                                   B2 = 5000,
                                                   B3 = 25000,
                                                   undercover_group_size = 1,
-                                                  n_calibration_pairs = 9 * nrow(response_matrix))
+                                                  n_calibration_pairs =  20000) # n_ntc * nrow(response_matrix))
 p <- sceptre3::plot_calibration_results(undercover_result)
 library(ggplot2)
-ggsave(filename = "~/Desktop/papalexi_undercover_grp1.pdf", plot = p, device = "pdf", scale = 0.9, width = 6, height = 5.5)
+
+
+
+sceptre3::run_sceptre_lowmoi(response_matrix = )
