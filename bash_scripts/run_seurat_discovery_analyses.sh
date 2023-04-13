4f2e741934f2a9a0d6ec309b92c89bdb1821fdff
Tim <tbarry2@andrew.cmu.edu>
2023-04-13 17:33:46 -0400

    prepare launch of seurat discovery analyses

--- bash_scripts/run_seurat_discovery_analyses.sh
+++ bash_scripts/run_seurat_discovery_analyses.sh
@@ -2,5 +2,5 @@ source ~/.research_config
 
 datasets=(schraivogel/enhancer_screen_chr11/gene schraivogel/enhancer_screen_chr8/gene papalexi/eccite_screen/gene frangieh/control/gene)
 for dataset in ${datasets[@]}; do
-  qsub run_seurat_discovery_analysis.sh $dataset
+  qsub -j y run_seurat_discovery_analysis.sh $dataset
 done
