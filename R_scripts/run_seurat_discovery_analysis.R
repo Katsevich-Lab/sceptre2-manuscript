514aa1b7e8eb10200c7770baad4f711fc14d66fd
Tim <tbarry2@andrew.cmu.edu>
2023-04-14 11:56:12 -0400

    add functionality to feature pairs for n cells

--- R_scripts/run_seurat_discovery_analysis.R
+++ R_scripts/run_seurat_discovery_analysis.R
@@ -9,9 +9,22 @@ response_odm <- lowmoi::load_dataset_modality(dataset_name)
 grna_dataset_name <- lowmoi::get_grna_dataset_name(dataset_name, "assignment")
 grna_odm <- lowmoi::load_dataset_modality(grna_dataset_name)
 response_grna_group_pairs <- generate_all_pairs(response_odm, grna_odm)
+
+sample_size_df <- readRDS(paste0(LOCAL_SCEPTRE2_DATA_DIR,
+                                 "results/dataset_sample_sizes/n_nonzero_cells_per_grna.rds")) |>
+  dplyr::filter(dataset_concat == dataset_name)
+ex_feat <- as.character(unique(sample_size_df$feature_id)[1])
+x <- sample_size_df |>
+  dplyr::filter(feature_id == ex_feat) |>
+  dplyr::group_by(target) |>
+  dplyr::summarize(count = sum(n_cells)) |>
+  dplyr::filter(count >= 7)
+
 res <- seurat_de(response_odm = response_odm,
                  grna_odm = grna_odm,
                  response_grna_group_pairs = response_grna_group_pairs)
+
+
 
 # save result
 f_name <- paste0("seurat_", gsub(pattern = "/", x = dataset_name, fixed = TRUE, replacement = "_"), "_res.rds")
