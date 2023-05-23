580948b06478bdf43c8ebd1d520229f66124231e
Tim <tbarry2@andrew.cmu.edu>
2023-05-23 10:25:26 -0400

    sample 10 genes

--- pipeline_launch_scripts/undercover/grp_size_1_extras_0523/grp_size_1.sh
+++ pipeline_launch_scripts/undercover/grp_size_1_extras_0523/grp_size_1.sh
@@ -14,6 +14,7 @@ nextflow run Katsevich-Lab/undercover-grna-pipeline -r main \
  --partition_count "1" \
  --is_partition_count_frac "false" \
  -profile standard \
+ -genes_to_subsample 10 \
  -with-trace
 
 # is_partition_count_frac = false for trial
