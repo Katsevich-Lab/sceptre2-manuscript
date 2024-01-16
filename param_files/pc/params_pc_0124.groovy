edbb538ca6256ce63317a79f7553f92c7b37b02b
Tim Barry <tbarry2@andrew.cmu.edu>
2024-01-16 16:41:59 -0500

    run sample pc analysis in which only seurat_de is applied

--- param_files/pc/params_pc_0124.groovy
+++ param_files/pc/params_pc_0124.groovy
@@ -1,11 +1,12 @@
-data_method_pairs_indiv = ["frangieh/co_culture/gene": ["schraivogel_method"],
-                           "frangieh/control/gene": ["schraivogel_method"],
-                           "frangieh/ifn_gamma/gene": ["schraivogel_method"],
-                           "papalexi/eccite_screen/gene": ["schraivogel_method"],
-                           "papalexi/eccite_screen/protein": ["schraivogel_method"],
-                           "schraivogel/enhancer_screen_chr11/gene": ["schraivogel_method"],
-                           "schraivogel/enhancer_screen_chr8/gene": ["schraivogel_method"],
-                           "simulated/experiment_2/gene": ["schraivogel_method"]
+/*
+data_method_pairs_indiv = ["frangieh/co_culture/gene": ["mimosca", "schraivogel_method"],
+                           "frangieh/control/gene": ["mimosca", "schraivogel_method"],
+                           "frangieh/ifn_gamma/gene": ["mimosca", "schraivogel_method"],
+                           "papalexi/eccite_screen/gene": ["mimosca", "schraivogel_method"],
+                           "papalexi/eccite_screen/protein": ["mimosca", "schraivogel_method"],
+                           "schraivogel/enhancer_screen_chr11/gene": ["mimosca", "schraivogel_method"],
+                           "schraivogel/enhancer_screen_chr8/gene": ["mimosca", "schraivogel_method"],
+                           "simulated/experiment_2/gene": ["mimosca", "schraivogel_method"]
 ]
 data_method_pairs_grouped = ["frangieh/co_culture/gene": ["seurat_de", "liscovitch_method", "weissman_method", "seurat_de_nb"],
                              "frangieh/control/gene": ["seurat_de", "liscovitch_method", "weissman_method", "seurat_de_nb"],
@@ -16,6 +17,18 @@ data_method_pairs_grouped = ["frangieh/co_culture/gene": ["seurat_de", "liscovit
                              "schraivogel/enhancer_screen_chr8/gene": ["seurat_de", "liscovitch_method", "weissman_method", "seurat_de_nb"]
                              // "simulated/experiment_2/gene": ["seurat_de", "liscovitch_method", "weissman_method", "seurat_de_nb"]
                             ]
+*/
+
+data_method_pairs_grouped = ["frangieh/co_culture/gene": ["seurat_de"],
+                             "frangieh/control/gene": ["seurat_de"],
+                             "frangieh/ifn_gamma/gene": ["seurat_de"],
+                             "papalexi/eccite_screen/gene": ["seurat_de"],
+                             "papalexi/eccite_screen/protein": ["seurat_de"],
+                             "schraivogel/enhancer_screen_chr11/gene": ["seurat_de"],
+                             "schraivogel/enhancer_screen_chr8/gene": ["seurat_de"]
+                             "simulated/experiment_2/gene": ["seurat_de"]
+                            ]
+
 row_names = ["frangieh/co_culture/gene",
              "frangieh/control/gene",
              "frangieh/ifn_gamma/gene",
@@ -31,7 +44,7 @@ col_names = ["schraivogel_method",
              "weissman_method",
              "sceptre",
              "seurat_de_nb"]
-             
+
 // SECOND, define a matrix indicating the amount of RAM to request for each dataset-method pair
 data_method_ram_matrix = [
                          [34, 13, 13, 35, 49, 8, 13], // frangieh/co_culture/gene
