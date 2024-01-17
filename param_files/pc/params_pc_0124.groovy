dfb5164c73480c978df1045729573a7e415a19f1
Tim Barry <tbarry2@andrew.cmu.edu>
2024-01-17 18:14:03 -0500

    add liscov method to list of methods

--- param_files/pc/params_pc_0124.groovy
+++ param_files/pc/params_pc_0124.groovy
@@ -21,14 +21,14 @@ data_method_pairs_grouped = ["frangieh/co_culture/gene": ["seurat_de", "liscovit
 
 data_method_pairs_indiv = []
 
-data_method_pairs_grouped = ["frangieh/co_culture/gene": ["seurat_de"],
-                             "frangieh/control/gene": ["seurat_de"],
-                             "frangieh/ifn_gamma/gene": ["seurat_de"],
-                             "papalexi/eccite_screen/gene": ["seurat_de"],
-                             "papalexi/eccite_screen/protein": ["seurat_de"],
-                             "schraivogel/enhancer_screen_chr11/gene": ["seurat_de"],
-                             "schraivogel/enhancer_screen_chr8/gene": ["seurat_de"],
-                             "simulated/experiment_2/gene": ["seurat_de"]
+data_method_pairs_grouped = ["frangieh/co_culture/gene": ["seurat_de", "liscovitch_method"],
+                             "frangieh/control/gene": ["seurat_de", "liscovitch_method"],
+                             "frangieh/ifn_gamma/gene": ["seurat_de", "liscovitch_method"],
+                             "papalexi/eccite_screen/gene": ["seurat_de", "liscovitch_method"],
+                             "papalexi/eccite_screen/protein": ["seurat_de", "liscovitch_method"],
+                             "schraivogel/enhancer_screen_chr11/gene": ["seurat_de", "liscovitch_method"],
+                             "schraivogel/enhancer_screen_chr8/gene": ["seurat_de", "liscovitch_method"],
+                             "simulated/experiment_2/gene": ["seurat_de", "liscovitch_method"]
                             ]
 
 row_names = ["frangieh/co_culture/gene",
