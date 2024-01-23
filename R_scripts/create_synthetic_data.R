08d7787caa4d02313120781a212a108fe1bd3091
Tim Barry <tbarry2@andrew.cmu.edu>
2024-01-23 14:16:21 -0500

    update create pc synth data and unif processing lowmoi

--- R_scripts/create_synthetic_data.R
+++ R_scripts/create_synthetic_data.R
@@ -78,69 +78,44 @@ convert_assign_list_to_sparse_odm(cell_barcodes = cell_barcodes,
 ###########
 # set the number of grnas
 set.seed(4)
-N_BATCHES <- 2
 N_PC_GRNAS <- 25
-N_NT_GRNAS <- 40
+N_NT_GRNAS <- 100
 N_GRNAS <- N_PC_GRNAS + N_NT_GRNAS 
 N_GENES <- N_GRNAS
-N_CELLS_PER_BATCH <- 10000
+N_CELLS <- 15000
+grna_assignments <- sample(x = seq(1, N_GRNAS), size = N_CELLS, replace = TRUE)
+mu_0s <- rgamma(n = N_GENES, shape = 2, rate = 0.5)
+mu_1s <- rgamma(n = N_GENES, shape = 2, rate = 0.5)
+thetas <- runif(n = N_GENES, min = 1, max = 25)
 
-# generate gene and grna ids
+# generate the gene expression matrix
+gene_expression_mat <- sapply(X = seq(1, N_GENES), FUN = function(i) {
+  alt_cell_idxs <- which(grna_assignments == i)
+  null_cell_idxs <- which(grna_assignments != i)
+  y_null <- MASS::rnegbin(length(null_cell_idxs), mu_0s[i], thetas[i])
+  y_alt <- MASS::rnegbin(length(alt_cell_idxs), mu_1s[i], thetas[i])
+  y <- integer(N_CELLS)
+  y[null_cell_idxs] <- y_null
+  y[alt_cell_idxs] <- y_alt
+  return(y)
+}) |> t()
+
+# generate the gRNA expression matrix
+grna_expression_mat <- sapply(X = grna_assignments, FUN = function(i) {
+  out <- numeric(length = N_GRNAS)
+  out[i] <- max(1, rpois(1, 100))
+  return(out)
+})
+
+# generate the gene ids, grna ids, and cell barcodes; append to the ondisc objects
+cell_barcodes <- paste0("cell-", seq(1, N_CELLS))
 gene_ids <- paste0("gene-", seq(1, N_GENES))
 grna_ids <- c(paste0("GENE-TARGET-", seq(1, N_PC_GRNAS)),
               paste0("NTC-", seq(1, N_GRNAS - N_PC_GRNAS)))
 
-syn_data <- lapply(X = seq(1, 2), FUN = function(batch_id) {
-  # assign the gRNAs to cells; the gRNAs are drawn according to different probabilities
-  grna_probs <- rbeta(n = N_GRNAS, shape1 = 1, shape2 = 1.5)
-  grna_probs <- grna_probs/sum(grna_probs)
-  grna_assignments <- sample(x = seq(1, N_GRNAS), size = N_CELLS_PER_BATCH, replace = TRUE, prob = grna_probs)
-  
-  # set the mean expression level of the genes; the PC genes have a different mean expression, while the remaining genes have the same mean expression
-  mu_0s <- mu_1s <- numeric(N_GENES)
-  mu_0s[seq(1, N_PC_GRNAS)] <- rgamma(n = N_PC_GRNAS, shape = 2, rate = 0.5)
-  mu_1s[seq(1, N_PC_GRNAS)] <- rgamma(n = N_PC_GRNAS, shape = 2, rate = 0.5)
-  mu_0s[seq(N_PC_GRNAS + 1, N_GENES)] <- mu_1s[seq(N_PC_GRNAS + 1, N_GENES)] <- rgamma(n = N_GENES - N_PC_GRNAS, shape = 2, rate = 0.5)
-  thetas <- runif(n = N_GENES, min = 1, max = 25)
-  
-  # generate the gene expression matrix
-  gene_expression_mat <- sapply(X = seq(1, N_GENES), FUN = function(i) {
-    alt_cell_idxs <- which(grna_assignments == i)
-    null_cell_idxs <- which(grna_assignments != i)
-    y_null <- MASS::rnegbin(length(null_cell_idxs), mu_0s[i], thetas[i])
-    y_alt <- MASS::rnegbin(length(alt_cell_idxs), mu_1s[i], thetas[i])
-    y <- integer(N_CELLS_PER_BATCH)
-    y[null_cell_idxs] <- y_null
-    y[alt_cell_idxs] <- y_alt
-    return(y)
-  }) |> t()
-  
-  # generate the gRNA expression matrix
-  grna_expression_mat <- sapply(X = grna_assignments, FUN = function(i) {
-    out <- numeric(length = N_GRNAS)
-    out[i] <- max(1, rpois(1, 100))
-    return(out)
-  })
-  
-  # append the row names
-  rownames(gene_expression_mat) <- gene_ids
-  rownames(grna_expression_mat) <- grna_ids
-  
-  # return the data
-  list(gene_expression_mat = gene_expression_mat, grna_expression_mat = grna_expression_mat, grna_assignments = grna_assignments)  
-})
-
-# combine the matrices into a single matrix
-gene_expression_mat <- cbind(syn_data[[1]]$gene_expression_mat, syn_data[[2]]$gene_expression_mat)
-grna_expression_mat <- cbind(syn_data[[1]]$grna_expression_mat, syn_data[[2]]$grna_expression_mat)
-grna_assignments <- c(syn_data[[1]]$grna_assignments, syn_data[[2]]$grna_assignments)
-
-# create the batch vector
-batch <- c(rep("b1", N_CELLS_PER_BATCH), rep("b2", N_CELLS_PER_BATCH)) |> factor()
-
-# generate the gene ids, grna ids, and cell barcodes; append to the ondisc objects
-cell_barcodes <- paste0("cell-", seq(1, N_BATCHES * N_CELLS_PER_BATCH))
+rownames(gene_expression_mat) <- gene_ids
 colnames(gene_expression_mat) <- cell_barcodes
+rownames(grna_expression_mat) <- grna_ids
 colnames(grna_expression_mat) <- cell_barcodes
 
 # initialize ODM objects
@@ -163,14 +138,10 @@ grna_odm <- create_ondisc_matrix_from_R_matrix(r_matrix = grna_expression_mat,
 grna_tbl <- data.frame(target_type = c(rep("gene", N_PC_GRNAS), rep("non-targeting", N_NT_GRNAS)),
                        target = c(paste0("gene-", seq(1, N_PC_GRNAS)), rep("non-targeting", N_NT_GRNAS)))
 
-# update the grna odm (with target info) and gene odm and grna odm (with batch info)
+# update the grna odm
 grna_odm <- grna_odm |>
-  mutate_feature_covariates(grna_tbl) |>
-  mutate_cell_covariates(batch = batch)
+  mutate_feature_covariates(grna_tbl)
 save_odm(odm = grna_odm, metadata_fp = paste0(sceptre2_dir, "data/simulated/experiment_2/grna_expression/metadata_orig.rds"))
-gene_odm <- gene_odm |>
-  mutate_cell_covariates(batch = batch)
-save_odm(odm = gene_odm, metadata_fp = paste0(sceptre2_dir, "data/simulated/experiment_2/gene/metadata_orig.rds"))
 
 # finally, create the matrix of grna assignments
 convert_assign_list_to_sparse_odm(cell_barcodes = cell_barcodes,
--- R_scripts/uniform_processing_lowmoi.R
+++ R_scripts/uniform_processing_lowmoi.R
@@ -10,41 +10,48 @@ papers <- c("frangieh",  "papalexi", "schraivogel", "simulated")
 FRAC_EXPRESSED_TRHESH <- 0.005
 
 # 1.i) Set the MIMOSCA formula objects
-mimosca_formula_objs <- list(frangieh = formula(~ n_nonzero + n_umis + 0),
-                             schraivogel = formula(~ n_nonzero + n_umis + batch + 0),
-                             papalexi = formula(~ n_nonzero + n_umis + bio_rep + p_mito + 0),
-                             liscovitch = formula(~ n_nonzero + n_fragments + 0),
-                             simulated = formula(~ n_nonzero + n_umis + 0))
+mimosca_formula_objs <- list(# frangieh = formula(~ n_nonzero + n_umis + phase + batch + 0),
+  frangieh = formula(~ n_nonzero + n_umis + 0),
+  schraivogel = formula(~ n_nonzero + n_umis + batch + 0),
+  papalexi = formula(~ n_nonzero + n_umis + bio_rep + p_mito + 0),
+  liscovitch = formula(~ n_nonzero + n_fragments + 0),
+  simulated = formula(~ n_nonzero + n_umis + 0))
 
-mimosca_formula_objs_protein <- list(frangieh = formula(~ n_umis + 0),
-                                     papalexi = formula(~ n_umis + bio_rep + p_mito + 0))
+mimosca_formula_objs_protein <- list(# frangieh = formula(~ n_umis + phase + batch + 0),
+  frangieh = formula(~ n_umis + 0),
+  papalexi = formula(~ n_umis + bio_rep + p_mito + 0))
 
 # 1.ii) Set the NB regression formula objects
-nb_regression_formula_objs <- list(frangieh = "~log(n_umis) + log(n_nonzero)",
-                                   schraivogel = "~ log(n_umis) + log(n_nonzero) + batch",
-                                   papalexi = "~ log(n_umis) + log(n_nonzero) + bio_rep + p_mito",
-                                   liscovitch = "~ log(n_fragments)",
-                                   simulated = "~log(n_umis) + log(n_nonzero)")
+nb_regression_formula_objs <- list(# frangieh = "~log(n_umis) + log(n_nonzero) + phase + batch",
+  frangieh = "~log(n_umis) + log(n_nonzero)",
+  schraivogel = "~ log(n_umis) + log(n_nonzero) + batch",
+  papalexi = "~ log(n_umis) + log(n_nonzero) + bio_rep + p_mito",
+  liscovitch = "~ log(n_fragments)",
+  simulated = "~log(n_umis) + log(n_nonzero)")
 
-nb_regression_formula_objs_protein <- list(frangieh = "~ offset(log(n_umis))",
-                                           papalexi = "~ offset(log(n_umis)) + bio_rep + p_mito")
+nb_regression_formula_objs_protein <- list(# frangieh = "~ offset(log(n_umis)) + phase + batch",
+  frangieh = "~ offset(log(n_umis))",
+  papalexi = "~ offset(log(n_umis)) + bio_rep + p_mito")
 
 # 1.iii) Set the sceptre formula objects
-sceptre_formula_objs <- list(frangieh = ~ log(response_n_umis) + log(response_n_nonzero),
-                             schraivogel = ~ log(response_n_umis) + log(response_n_nonzero) + batch,
-                             papalexi = ~ log(response_n_umis) + log(response_n_nonzero) + bio_rep + p_mito,
-                             liscovitch = ~ log(response_n_fragments),
-                             simulated = ~ log(response_n_umis) + log(response_n_nonzero))
+sceptre_formula_objs <- list(# frangieh = ~ log(response_n_umis) + log(response_n_nonzero) + phase + batch,
+  frangieh = ~ log(response_n_umis) + log(response_n_nonzero),
+  schraivogel = ~ log(response_n_umis) + log(response_n_nonzero) + batch,
+  papalexi = ~ log(response_n_umis) + log(response_n_nonzero) + bio_rep + p_mito,
+  liscovitch = ~ log(response_n_fragments),
+  simulated = ~ log(response_n_umis) + log(response_n_nonzero))
 
-sceptre_formula_objs_protein <- list(frangieh = ~ log(response_n_umis),
-                                     papalexi = ~ log(response_n_umis) + bio_rep + p_mito)
+sceptre_formula_objs_protein <- list(# frangieh = ~ log(response_n_umis) + phase + batch,
+  frangieh = ~ log(response_n_umis),
+  papalexi = ~ log(response_n_umis) + bio_rep + p_mito)
 
 # 1. iv) Set the global formula object (for use in the sceptre pipeline)
-global_formula_objs <- list(frangieh = ~ log(gene_n_umis) + log(gene_n_nonzero),
-                            schraivogel = ~ log(gene_n_umis) + log(gene_n_nonzero) + batch,
-                            papalexi = ~ log(gene_n_umis) + log(gene_n_nonzero) + bio_rep + p_mito,
-                            liscovitch = ~ log(gene_n_fragments),
-                            simulated = ~ log(gene_n_umis) + log(gene_n_nonzero))
+global_formula_objs <- list(# frangieh = ~ log(gene_n_umis) + log(gene_n_nonzero) + phase + batch,
+  frangieh = ~ log(gene_n_umis) + log(gene_n_nonzero),
+  schraivogel = ~ log(gene_n_umis) + log(gene_n_nonzero) + batch,
+  papalexi = ~ log(gene_n_umis) + log(gene_n_nonzero) + bio_rep + p_mito,
+  liscovitch = ~ log(gene_n_fragments),
+  simulated = ~ log(gene_n_umis) + log(gene_n_nonzero))
 
 # 2) loop over datasets, loading all modalities
 for (paper in papers) {
@@ -56,7 +63,7 @@ for (paper in papers) {
     multimodal_metadata_fp <- paste0(paper_dir, dataset, "/multimodal_metadata.rds")
     if (file.exists(multimodal_metadata_fp)) file.remove(multimodal_metadata_fp)
     mm_odm <- lowmoi::read_all_modalities(paper, dataset)
-
+    
     # i. perform cell QC; restrict attention to 1 grna/cell and "passed_qc" cells (if applicable)
     global_cell_covariates <- mm_odm |> get_cell_covariates()
     cell_logical_v <- global_cell_covariates$grna_assignment_n_nonzero == 1
@@ -66,7 +73,7 @@ for (paper in papers) {
       cell_logical_v <- cell_logical_v & passed_qc
     }
     mm_odm_sub <- mm_odm[,cell_logical_v]
-
+    
     # ii. perform feature QC
     modalities <- names(mm_odm_sub@modalities)
     # grna assignment modality: keep features expressed in N_CELLS_PER_GRNA_THRESH cells. Also, add a "grna_assigned" column to the cell covariate matrix.
@@ -82,7 +89,7 @@ for (paper in papers) {
         mutate_feature_covariates(target = ifelse(is.na(known_effect), target, known_effect),
                                   known_effect = NULL)
     }
-
+    
     # grna expression modality (if applicable): keep the same features as above
     if ("grna_expression" %in% modalities) {
       grna_expression_modality <- get_modality(mm_odm_sub, "grna_expression")
@@ -91,7 +98,7 @@ for (paper in papers) {
           mutate_feature_covariates(target = known_effect, known_effect = NULL)
       }
     }
-
+    
     # response modalities: keep features expressed in FRAC_EXPRESSED_TRHESH of cells
     remaining_modalities <- modalities[!(modalities %in% c("grna_assignment", "grna_expression"))]
     for (modality in remaining_modalities) {
@@ -99,7 +106,7 @@ for (paper in papers) {
       feats_to_keep <- get_highly_expressed_features(modality_odm, FRAC_EXPRESSED_TRHESH)
       mm_odm_sub@modalities[[modality]] <- modality_odm[feats_to_keep,]
     }
-
+    
     # iii. perform feature ID cleanup; remove underscores and replace with dashes for all features
     for (modality in modalities) {
       modality_odm <- get_modality(mm_odm_sub, modality)
@@ -110,7 +117,7 @@ for (paper in papers) {
       }
       mm_odm_sub@modalities[[modality]] <- modality_odm
     }
-
+    
     # iv. add the mimosca/nb formula objects to each response modality
     for (modality in remaining_modalities) {
       modality_odm <- get_modality(mm_odm_sub, modality)
@@ -119,22 +126,16 @@ for (paper in papers) {
         modality_odm@misc[["nb_regression_formula"]] <- nb_regression_formula_objs_protein[[paper]]
         modality_odm@misc[["sceptre_formula"]] <- sceptre_formula_objs_protein[[paper]]
       } else {
-          modality_odm@misc[["mimosca_formula"]] <- mimosca_formula_objs[[paper]]
-          modality_odm@misc[["nb_regression_formula"]] <- nb_regression_formula_objs[[paper]]
-          modality_odm@misc[["sceptre_formula"]] <- sceptre_formula_objs[[paper]]
-          # special case: dataset == "experiment_2", include batch
-          if (dataset == "experiment_2") {
-            modality_odm@misc[["mimosca_formula"]] <- formula(~n_nonzero + n_umis + batch + 0)
-            modality_odm@misc[["nb_regression_formula"]] <- "~log(n_umis) + log(n_nonzero) + batch"
-            modality_odm@misc[["sceptre_formula"]] <- formula(~log(response_n_umis) + log(response_n_nonzero) + batch)
-          }
+        modality_odm@misc[["mimosca_formula"]] <- mimosca_formula_objs[[paper]]
+        modality_odm@misc[["nb_regression_formula"]] <- nb_regression_formula_objs[[paper]]
+        modality_odm@misc[["sceptre_formula"]] <- sceptre_formula_objs[[paper]]
       }
       mm_odm_sub@modalities[[modality]] <- modality_odm
     }
-
+    
     # Write all modalities
     lowmoi::save_all_modalities(multimodal_odm = mm_odm_sub, paper = paper, dataset = dataset, metadata_file_name = "metadata_qc.rds")
-
+    
     # v. create a multimodal ondisc matrix free of redundancy and write
     mm_odm_sub_proc <- lowmoi::process_multimodal_odm(mm_odm_sub)
     mm_odm_sub_proc@global_misc[["formula"]] <- global_formula_objs[[paper]]
@@ -172,20 +173,20 @@ for (paper in papers) {
     
     gene_modality <- mm_odm_sub_proc |> get_modality("gene")
     # vii. write the positive control pairs (at the level of the paper-dataset)
-    if (paper %in% c("frangieh", "papalexi", "simulated")) {
+    if (paper %in% c("frangieh", "papalexi")) {
       # grouped pairs
       targets <- intersect(grna_feature_df |> dplyr::pull(target),
                            gene_modality |> ondisc::get_feature_ids())
       pc_pairs <- data.frame(grna_group = targets, response_id = targets)
       saveRDS(pc_pairs, file = paste0(paper_dir, dataset, "/gene/pos_control_pairs_grouped.rds"))
     }
-
+    
     if (paper == "schraivogel") {
       targets <- intersect(grna_feature_df |> dplyr::pull(known_effect),
                            gene_modality |> ondisc::get_feature_ids())
       ungroup_pairs_all <- grna_feature_df |>
         dplyr::filter(known_effect %in% targets)
-
+      
       # first, grouped pairs
       pc_pairs <- ungroup_pairs_all |>
         dplyr::select(grna_group = target, response_id = known_effect) |>
@@ -194,7 +195,7 @@ for (paper in papers) {
       rownames(pc_pairs) <- NULL
       saveRDS(pc_pairs, file = paste0(paper_dir, dataset, "/gene/pos_control_pairs_grouped.rds"))
     }
-
+    
     # finally, do the papalexi protein data
     if (paper == "papalexi") {
       grna_assignment_modality <- mm_odm_sub_proc |> get_modality("grna_assignment")
@@ -202,13 +203,13 @@ for (paper in papers) {
       x <- grna_assignment_modality |>
         get_feature_covariates() |>
         dplyr::filter(!is.na(known_protein_effect),
-               n_nonzero > 0)
+                      n_nonzero > 0)
       pos_control_group <- x |>
         dplyr::select(target, known_protein_effect) |>
         dplyr::distinct() |>
         dplyr::rename("grna_group" = "target", "response_id" = "known_protein_effect")
       row.names(pos_control_group) <- NULL
-
+      
       saveRDS(pos_control_group,
               file = paste0(paper_dir, dataset, "/protein/pos_control_pairs_grouped.rds"))
     }
