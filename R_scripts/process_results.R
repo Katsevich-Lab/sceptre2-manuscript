dab0fe6594fe8d2016ceb273dd4737a09e26cde8
timothy-barry <tbarry2@andrew.cmu.edu>
2024-04-13 21:04:58 -0400

    update process results

--- R_scripts/process_results.R
+++ R_scripts/process_results.R
@@ -16,16 +16,15 @@ saveRDS(object = undercover_res_extra_processed,
         paste0(sceptre2_results_dir, "undercover_grna_analysis/undercover_result_grp_1_extras_0523_processed.rds"))
 
 # pc result
-pc_res <- readRDS(paste0(sceptre2_results_dir, "positive_control_analysis/pc_results_0124.rds"))
+pc_res <- readRDS(paste0(sceptre2_results_dir, "positive_control_analysis/pc_results_0523.rds"))
 pc_res_processed <- suppressWarnings(process_pc_result(pc_res, sample_size_df))
 saveRDS(object = pc_res_processed,
-        file = paste0(sceptre2_results_dir, "positive_control_analysis/pc_results_0124_processed.rds"))
+        file = paste0(sceptre2_results_dir, "positive_control_analysis/pc_results_0523_processed.rds"))
 
 pc_res_sceptre_unfiltered <- readRDS(paste0(sceptre2_results_dir, "positive_control_analysis/pc_results_sceptre_unfiltered_0523.rds"))
 pc_res_sceptre_unfiltered_processed <- suppressWarnings(process_pc_result(pc_res_sceptre_unfiltered, sample_size_df))
 saveRDS(object = pc_res_sceptre_unfiltered_processed,
         file = paste0(sceptre2_results_dir, "positive_control_analysis/pc_results_sceptre_unfiltered_0523_processed.rds"))
-
 
 # resampling results
 resampling_res <- readRDS(paste0(sceptre2_results_dir,
@@ -35,6 +34,6 @@ saveRDS(object = resampling_res_processed, paste0(sceptre2_results_dir,
                                                   "resampling_distributions/seurat_resampling_at_scale_processed.rds"))
 
 # discovery result
-discovery_res <- readRDS(paste0(sceptre2_results_dir, "discovery_analyses/discovery_results_0423.rds"))
+discovery_res <- readRDS(paste0(sceptre2_results_dir, "discovery_analyses/discovery_results_0523.rds"))
 discovery_res_processed <- suppressWarnings(process_pc_result(discovery_res, sample_size_df))
-saveRDS(discovery_res_processed, paste0(sceptre2_results_dir, "discovery_analyses/discovery_results_0423_processed.rds"))
+saveRDS(discovery_res_processed, paste0(sceptre2_results_dir, "discovery_analyses/discovery_results_0523_processed.rds"))
